5959220a1ca006276b736cf39e402beab636dda3
Andrey Miskov <amiskov@gmail.com>
2020-07-25 14:23:05 +0300

    Updates

---
+++ precache-manifest.2f4a07244303ed3224c0ce5b5dc71351.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/expenses/favicon.ico"
+  },
+  {
+    "revision": "02310a21e8385fc9c34b2528ab948b76",
+    "url": "/expenses/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/expenses/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/expenses/manifest.json"
+  },
+  {
+    "revision": "e88cd108953e16baa000",
+    "url": "/expenses/static/css/main.42ec3327.chunk.css"
+  },
+  {
+    "revision": "e88cd108953e16baa000",
+    "url": "/expenses/static/js/main.e88cd108.chunk.js"
+  },
+  {
+    "revision": "faa03718e73af563b2d0",
+    "url": "/expenses/static/js/runtime~main.faa03718.js"
+  },
+  {
+    "revision": "de195ff2ea4eb00b4f85",
+    "url": "/expenses/static/js/vendors~main.de195ff2.chunk.js"
+  }
+]);
